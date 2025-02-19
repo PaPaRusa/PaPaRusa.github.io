f12b93fa4da37a4ddb5faa775d69ef980b187c53
PaPaRusa <77152942+PaPaRusa@users.noreply.github.com>
2025-02-19 15:09:19 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
