99272d5b05e9751ac6a452c00411d983ee2bd411
kfir <kfirhason342@gmail.com>
2025-03-04 15:45:28 +0200

    changes

---
+++ public/script.js
@@ -0,0 +1,5 @@
+// Toggle Sidebar Menu
+function toggleMenu() {
+    let sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active"); // Add or remove the 'active' class
+}
